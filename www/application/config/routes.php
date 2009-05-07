be9da9acb2b76ba6afb4e022e6f26b44cdeea28e
argoroots <argo@roots.ee>
2009-05-07 14:01:14 +0000

    Konf ja info kontroller
    git-svn-id: http://argo.unfuddle.com/svn/argo_engine/trunk@4 2b349b5e-f550-4985-9274-b15e38e8fe12

--- www/application/config/routes.php
+++ www/application/config/routes.php
@@ -40,7 +40,7 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "info";
 $route['scaffolding_trigger'] = "";
 
 
---
+++ www/application/controllers/info.php
@@ -0,0 +1,24 @@
+<?php
+
+class Info extends Controller {
+
+	function __construct() {
+		parent::Controller();
+
+		//$this->output->enable_profiler(TRUE);      
+
+	}
+
+	function index() {
+
+		echo 'OK';
+
+	}
+
+	function php() {
+		phpinfo();
+	}
+
+}	
+	
+?>
