3df04966cdc1a5487ee06b46cac2f8966c9fc525
argoroots <argo@roots.ee>
2009-06-02 14:06:09 +0000

    Õigesse formaati
    git-svn-id: http://argo.unfuddle.com/svn/argo_engine/trunk@11 2b349b5e-f550-4985-9274-b15e38e8fe12

--- www/application/controllers/info.php
+++ www/application/controllers/info.php
@@ -1,24 +1,22 @@
-<?php
-
-class Info extends Controller {
-
-	function __construct() {
-		parent::Controller();
-
-		//$this->output->enable_profiler(TRUE);      
-
-	}
-
-	function index() {
-
-		echo 'OK';
-
-	}
-
-	function php() {
-		phpinfo();
-	}
-
-}	
-	
-?>
+<?php
+
+class Info extends Controller {
+
+	function __construct() {
+		parent::Controller();
+
+		//$this->output->enable_profiler(TRUE);
+
+	}
+
+	function index() {
+ 		echo 'OK';
+	}
+
+	function php() {
+		phpinfo();
+	}
+
+}
+
+?>
